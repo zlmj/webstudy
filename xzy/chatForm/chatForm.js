99721e87c898e4514823624dea9112d3a2f2af29
xzhongy <914890169@qq.com>
2019-11-04 00:24:09 +0800

    Add files via upload

---
+++ xzy/chatForm/chatForm.js
@@ -0,0 +1,136 @@
+﻿//当前登录用户
+var currentUser = { "userName": "向中燕", "photo": "../resource/image/user1.jpg" };
+
+//发出消息
+function sendMsg() {
+    var input = document.getElementById("inputMsg");
+    var inputMsg = input.value;
+    if (inputMsg == null || inputMsg == '') {
+        return;
+    }
+
+    var html = createSendedMsg(inputMsg);
+    var msgArea = document.getElementById("messageArea");
+    msgArea.appendChild(html);
+    input.value = null;
+    msgArea.scrollTop = msgArea.scrollHeight;
+    input.focus();
+}
+
+//用html显示发出的消息
+function createSendedMsg(msg) {
+    var nowtime = new Date();
+    var time = nowtime.Format("yyyy-MM-dd HH:mm:ss");
+
+    var item = document.createElement("div");
+    item.className = 'sendMsgItem';
+    item.innerHTML = createMsgHtml(msg, currentUser.userName, currentUser.photo, time);
+    return item;
+}
+
+//用html显示收到的消息
+function createRecivedMsg(msg, userName, photo, time) {
+    var item = document.createElement("div");
+    item.className = 'reciveMsgItem';
+    item.innerHTML = createMsgHtml(msg, userName, photo, time);
+    return item;
+}
+
+//消息的html
+function createMsgHtml(msg, userName, photo, time) {
+    return '<div class="msgTitle">' + userName + '&nbsp;&nbsp;&nbsp;&nbsp;' + time + '</div>'
+        + '<div>'
+        + '<img class="photo" src="' + photo + '" />'
+        + '<div class="msgContent">' + msg + '</div>'
+        + '<div class="clear"></div>'
+        + '</div>';
+}
+
+//加载历史消息
+function loadHistoryData(historyMsg) {
+    $("#messageArea").html("");
+    if (historyMsg.length == 0) {
+        return;
+    }
+
+    var msgArea = document.getElementById("messageArea");
+
+    for (var msgItem of historyMsg) {
+        var html = null;
+
+        if (msgItem.userName == currentUser.userName) {
+            html = createSendedMsg(msgItem.msg);
+        }
+        else {
+            var photo = '../resource/image/user1.jpg';
+            html = createRecivedMsg(msgItem.msg, msgItem.userName, photo, msgItem.time);
+        }
+
+        msgArea.appendChild(html);
+    }
+}
+
+function receiveSystemMsg(msg) {
+    var html = '<span>系统消息：' + msg + '</span>';
+
+    var item = document.createElement("div");
+    item.className = 'systemMsg';
+    item.innerHTML = html;
+
+    var msgArea = document.getElementById("messageArea");
+    msgArea.appendChild(item);
+
+    return item;
+}
+
+function inputKeyDown() {
+    if (window.event.keyCode == 13) {
+        sendMsg();
+    }
+}
+
+window.onload = function () {
+    document.getElementById("inputMsg").focus();
+}
+
+$(document).ready(function () {
+    loadChatList();
+    registEvent();
+});
+
+function loadChatList() {
+    var msgText = "";
+    for (var i = 0; i < chatListData.length; i++) {
+        var item = chatListData[i];
+        msgText += '<li class="person">'
+            + '<img class="photo" src = "' + item.img + '" alt = "photo" />'
+            + '<span class="chatID" >' + item.chatID + '</span>'
+            + '<span class="userName">' + item.title + '</span>'
+            + '<span class="time">' + item.lastTime + '</span>'
+            + '<div class="previewMsg">' + item.lastMsg + '</div>'
+            + '</li>';
+    }
+
+    $("#personArea").html(msgText);
+}
+
+function registEvent() {
+    $(".person").click(function () {
+        $(".active").removeClass("active");
+        $(this).addClass("active");
+
+        var id = $(".active .chatID").html();
+        var msgItem;
+        if (id == "001") {
+            msgItem = msgData_001;
+        }
+        else if (id == "002") {
+            msgItem = msgData_002;
+        }
+        else if (id == "003") {
+            msgItem = msgData_003;
+        }
+
+        loadHistoryData(msgItem);
+    });
+}
---
+++ xzy/resource/testData/chatListData.js
@@ -0,0 +1,62 @@
+var chatListData = [{
+    "chatID": "001",
+    "title": "css学习小组",
+    "img": "../resource/image/user1.jpg",
+    "lastTime": "10-20",
+    "lastMsg": "css有3种布局模型，分别是：流布局，浮动布局，和层布局。"
+}, {
+    "chatID": "002",
+    "title": "html学习小组",
+    "img": "../resource/image/user1.jpg",
+    "lastTime": "10-19",
+    "lastMsg": "html用于定位元素的属性有class和id"
+}
+    , {
+    "chatID": "003",
+    "title": "js学习小组",
+    "img": "../resource/image/user1.jpg",
+    "lastTime": "10-18",
+    "lastMsg": "最新的js版本是ECMAScript 6"
+}]
+
+var msgData_001 = [
+    {
+        "userName": "向中燕",
+        "time": "2019-10-20 18:25",
+        "msg": "请问css有几种布局模型？",
+        "img": "../resource/image/user1.jpg"
+    }, {
+        "userName": "李雷",
+        "time": "2019-10-20 18:26",
+        "msg": "css有3种布局模型，分别是：流布局，浮动布局，和层布局。",
+        "img": "../resource/image/user1.jpg"
+    }
+];
+
+var msgData_002 = [
+    {
+        "userName": "向中燕",
+        "time": "2019-10-19 18:34",
+        "msg": "html用什么方式定位元素？",
+        "img": "../resource/image/user1.jpg"
+    }, {
+        "userName": "李雷",
+        "time": "2019-10-19 18:35",
+        "msg": "html通过属性class和id定位元素",
+        "img": "../resource/image/user1.jpg"
+    }
+]
+
+var msgData_003 = [
+    {
+        "userName": "向中燕",
+        "time": "2019-10-18 18:40",
+        "msg": "定义js语法的最新的标准是？",
+        "img": "../resource/image/user1.jpg"
+    }, {
+        "userName": "李雷",
+        "time": "2019-10-18 18:41",
+        "msg": "最新的js版本是ECMAScript 6",
+        "img": "../resource/image/user1.jpg"
+    }
+]
