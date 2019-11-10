1d3e8da38f4254e1cd51ff214f7737e5c7d82fc1
xzhongy <914890169@qq.com>
2019-11-11 01:47:28 +0800

    Add files via upload

---
+++ xzy_11_01/chatForm/chatForm.js
@@ -0,0 +1,42 @@
+﻿window.onload = function () {
+    new Vue({
+        el: "#wrapper"
+        , data: {
+            allPerson: chatListData
+            , currentMsgID: "001"
+            , msgList: msgHistory["001"]
+            , currentUser: "向中燕"
+            , inputValue: ""
+        }
+        , methods: {
+            changeActive: function (id) {
+                this.currentMsgID = id;
+                this.msgList = msgHistory[id];
+            }
+            , sendMsg: function () {
+                if (this.inputValue == null || this.inputValue == "") {
+                    return;
+                }
+
+                var nowtime = new Date();
+                var time = nowtime.Format("yyyy-MM-dd HH:mm:ss");
+
+                var newMsg = {};
+                newMsg.userName = this.currentUser;
+                newMsg.time = time;
+                newMsg.img = "../resource/image/user1.jpg";
+                newMsg.msg = this.inputValue;
+
+                this.msgList.push(newMsg);
+                this.inputValue = null;
+                
+                var msgArea = document.getElementById("messageArea");
+                msgArea.scrollTop = msgArea.scrollHeight;
+                var input = document.getElementById("inputMsg");
+                input.focus();
+            }
+        }
+    });
+
+    document.getElementById("inputMsg").focus();
+}
