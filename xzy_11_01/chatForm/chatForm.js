0e9ad76e6704579ee3d5d140745e6f79622fde4f
xzhongy <914890169@qq.com>
2019-11-11 19:06:30 +0800

    Update chatForm.js

--- xzy_11_01/chatForm/chatForm.js
+++ xzy_11_01/chatForm/chatForm.js
@@ -1,41 +1,47 @@
-﻿window.onload = function () {
-    new Vue({
-        el: "#wrapper"
-        , data: {
-            allPerson: chatListData
-            , currentMsgID: "001"
-            , msgList: msgHistory["001"]
-            , currentUser: "向中燕"
-            , inputValue: ""
-        }
-        , methods: {
-            changeActive: function (id) {
-                this.currentMsgID = id;
-                this.msgList = msgHistory[id];
-            }
-            , sendMsg: function () {
+window.onload = function() {
+    var vm = new Vue({
+        el: "#wrapper",
+        data: {
+            loginUser: "向中燕",                //登陆用户名
+            allPerson: chatListData,            //所有讨论组
+            activePersonID: "001",              //选中的讨论组        
+            activeChatMsg: msgHistory["001"],   //讨论组的历史消息
+            inputValue: null,                   //消息输入框的内容
+            searchTxt: null                     //搜索框的内容
+        },
+        methods: {
+            //切换焦点行
+            changeActive: function(id) {
+                this.activePersonID = id;
+                this.activeChatMsg = msgHistory[id];
+            },
+            //发消息
+            sendMsg: function() {
                 if (this.inputValue == null || this.inputValue == "") {
                     return;
                 }
-
                 var nowtime = new Date();
                 var time = nowtime.Format("yyyy-MM-dd HH:mm:ss");
 
                 var newMsg = {};
-                newMsg.userName = this.currentUser;
+                newMsg.userName = this.loginUser;
                 newMsg.time = time;
                 newMsg.img = "../resource/image/user1.jpg";
                 newMsg.msg = this.inputValue;
 
-                this.msgList.push(newMsg);
+                this.activeChatMsg.push(newMsg);
                 this.inputValue = null;
-                
-                var msgArea = document.getElementById("messageArea");
-                msgArea.scrollTop = msgArea.scrollHeight;
+                                
                 var input = document.getElementById("inputMsg");
                 input.focus();
             }
         }
+    });
+
+    //发消息后，滚动动条到底。
+    vm.$watch('activeChatMsg', function (nval, oval) {
+        var msgArea = document.getElementById("messageArea");
+        msgArea.scrollTop = msgArea.scrollHeight;
     });
 
     document.getElementById("inputMsg").focus();
